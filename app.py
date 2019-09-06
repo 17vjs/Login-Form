3d469dd48d2afdd494c6e3af8c1193e9067a7ca0
17vjs <vijaysffvf@gmail.com>
2019-09-07 02:11:28 +0530

    added app.py and index.html

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, redirect, url_for, request, render_template, abort
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/success/<name>/')
+def success(name):
+    return 'welcome %s' % name
+
+
+@app.route('/login/', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form['uname']
+        password = request.form['psw']
+        if user == 'admin' and password == 'admin':
+            return redirect(url_for('success', name=user))
+        else:
+            abort(401)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
